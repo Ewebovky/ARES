c89a0d7f92982a514dee484f0477e0bfcf99086b
Václav Pavlů <pavlu@ewebovky.cz>
2024-02-16 14:19:52 +0100

    Add files via upload

---
+++ src/ARES.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Ewebovky\ARES;
+
+use RestClient;
+
+const BASE_URL = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest";
+
+class ARES
+{
+    public function searchSubjects(string|int $search)
+    {
+        if ($this->verifyIC($search)) {
+            return array($this->vratEkonomickySubjekt($search));
+        } else {
+            return $this->vyhledejEkonomickeSubjekty($search);
+        }
+    }
+
+    public function vratEkonomickySubjekt(int $ico)
+    {
+        $api = new RestClient(['base_url' => BASE_URL]);
+
+        $result = $api->get("ekonomicke-subjekty/" . $ico);
+
+        if ($result->info->http_code == 200) {
+            return $result->decode_response();
+        }
+
+        return $result;
+    }
+
+    public function vyhledejEkonomickeSubjekty(string $company)
+    {
+        $api = new RestClient([
+            'base_url' => BASE_URL,
+        ]);
+
+        $headers = [
+            'accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ];
+
+        $parameters = [
+            'obchodniJmeno' => $company
+        ];
+
+        $result = $api->post("ekonomicke-subjekty/vyhledat", json_encode($parameters), $headers);
+
+        if ($result->info->http_code == 200) {
+            return $result->decode_response()->ekonomickeSubjekty;
+        }
+
+        return $result;
+    }
+
+    public function verifyIC($ic)
+    {
+        $ic = preg_replace('#\s+#', '', $ic);
+
+        if (!preg_match('#^\d{8}$#', $ic)) {
+            return false;
+        }
+
+        // kontrolní součet
+        $a = 0;
+        for ($i = 0; $i < 7; $i++) {
+            $a += $ic[$i] * (8 - $i);
+        }
+
+        $a = $a % 11;
+        if ($a === 0) {
+            $c = 1;
+        } elseif ($a === 1) {
+            $c = 0;
+        } else {
+            $c = 11 - $a;
+        }
+
+        return (int) $ic[7] === $c;
+    }
+
+    public function overitDIC($dic)
+    {
+        $soap = new \SoapClient('http://adisrws.mfcr.cz/adistc/axis2/services/rozhraniCRPDPH.rozhraniCRPDPHSOAP?wsdl');
+
+        return $soap->getStatusNespolehlivyPlatce(['dic' => $dic]);
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ewebovky/ares',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ewebovky/ares' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tcdent/php-restclient' => array(
+            'pretty_version' => '0.1.8.4',
+            'version' => '0.1.8.4',
+            'reference' => '4caee8a72e356f827ac404c2cfe2965e824f192b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tcdent/php-restclient',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
